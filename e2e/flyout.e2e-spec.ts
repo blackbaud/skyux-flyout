d87323e67ee61ace99fdf6f60766aa2e54549cc0
Alex <alex.kingman@blackbaud.com>
2020-11-06 15:18:08 -0500

    Modern theme for flyout component (#105)
    * init commit
    
    * Modern theme for flyout component
    
    * fixed e2e. added dark background color to flyout
    
    * reset baselines
    
    * upgraded theme
    
    * fixed test
    
    * added elevation class
    
    * added sky-margin-inline-sm class to buttons
    
    * Added keyboard controls for flyout
    
    * added output
    
    * PR feedback
    
    * PR feedback
    
    * code style
    
    * PR feedback
    
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>

--- e2e/flyout.e2e-spec.ts
+++ e2e/flyout.e2e-spec.ts
@@ -6,158 +6,194 @@ import {
 
 import {
   expect,
-  SkyHostBrowser
+  SkyHostBrowser,
+  SkyVisualThemeSelector
 } from '@skyux-sdk/e2e';
 
+import {
+  SkyHostBrowserBreakpoint
+} from '@skyux-sdk/e2e/host-browser/host-browser-breakpoint';
+
 describe('Flyout', () => {
-  it('should match previous screenshot', (done) => {
-    SkyHostBrowser.get('visual/flyout');
-    SkyHostBrowser.setWindowBreakpoint('lg');
-    element(by.css('.sky-btn-primary')).click();
-    expect('body').toMatchBaselineScreenshot(done, {
-      screenshotName: 'flyout-lg'
-    });
-    element(by.css('.sky-flyout .sky-flyout-btn-close')).click();
-  });
 
-  it('should match previous screenshot (screen: xs)', (done) => {
-    SkyHostBrowser.get('visual/flyout');
-    SkyHostBrowser.setWindowBreakpoint('xs');
-    element(by.css('.sky-btn-primary')).click();
-    expect('body').toMatchBaselineScreenshot(done, {
-      screenshotName: 'flyout-xs'
-    });
-    element(by.css('.sky-flyout .sky-flyout-btn-close')).click();
-  });
+  //#region helpers
+  let currentTheme: string;
+  let currentThemeMode: string;
 
-  it('should handle absolutely positioned items inside the flyout', (done) => {
-    SkyHostBrowser.get('visual/flyout');
-    SkyHostBrowser.setWindowBreakpoint('lg');
-    element(by.css('.sky-btn-primary')).click();
-    browser.sleep(250);
+  async function selectTheme(theme: string, mode: string): Promise<void> {
+    currentTheme = theme;
+    currentThemeMode = mode;
+
+    return SkyVisualThemeSelector.selectTheme(theme, mode);
+  }
+
+  function getScreenshotName(name: string): string {
+    if (currentTheme) {
+      name += '-' + currentTheme;
+    }
+
+    if (currentThemeMode) {
+      name += '-' + currentThemeMode;
+    }
+
+    return name;
+  }
+
+  function openDropdown(): void {
     element(by.css('.sky-flyout .sky-dropdown-button')).click();
     SkyHostBrowser.moveCursorOffScreen();
-    expect('body').toMatchBaselineScreenshot(done, {
-      screenshotName: 'flyout-absolute-lg'
-    });
-    element(by.css('.sky-flyout .sky-flyout-btn-close')).click();
-  });
+  }
 
-  it('should handle absolutely positioned items inside the flyout (screen: xs)', (done) => {
-    SkyHostBrowser.get('visual/flyout');
-    SkyHostBrowser.setWindowBreakpoint('xs');
+  function validateFlyout(size: SkyHostBrowserBreakpoint, done: DoneFn): void {
+    SkyHostBrowser.setWindowBreakpoint(size);
+    element(by.css('.sky-btn-primary')).click();
+    expect('body').toMatchBaselineScreenshot(done, {
+      screenshotName: getScreenshotName(`flyout-${size}`)
+    });
+  }
+
+  function validateDropdownInFlyout(size: SkyHostBrowserBreakpoint, done: DoneFn): void {
+    SkyHostBrowser.setWindowBreakpoint(size);
     element(by.css('.sky-btn-primary')).click();
     browser.sleep(250);
-    element(by.css('.sky-flyout .sky-dropdown-button')).click();
-    SkyHostBrowser.moveCursorOffScreen();
+    openDropdown();
     expect('body').toMatchBaselineScreenshot(done, {
-      screenshotName: 'flyout-absolute-xs'
+      screenshotName: getScreenshotName(`flyout-absolute-${size}`)
     });
-    element(by.css('.sky-flyout .sky-flyout-btn-close')).click();
-  });
+  }
 
-  it('should match previous screenshot when row iterators are enabled', (done) => {
-    SkyHostBrowser.get('visual/flyout');
-    SkyHostBrowser.setWindowBreakpoint('lg');
+  function validateIteratorButtons(size: SkyHostBrowserBreakpoint, done: DoneFn): void {
+    SkyHostBrowser.setWindowBreakpoint(size);
     element(by.css('#open-flyout-with-iterators')).click();
     expect('body').toMatchBaselineScreenshot(done, {
-      screenshotName: 'flyout-iterators-lg'
+      screenshotName: getScreenshotName(`flyout-iterators-${size}`)
     });
-    element(by.css('.sky-flyout .sky-flyout-btn-close')).click();
-  });
+  }
 
-  it('should match previous screenshot when row iterators are enabled (screen: xs)', (done) => {
-    SkyHostBrowser.get('visual/flyout');
-    SkyHostBrowser.setWindowBreakpoint('xs');
-    element(by.css('#open-flyout-with-iterators')).click();
-    expect('body').toMatchBaselineScreenshot(done, {
-      screenshotName: 'flyout-iterators-xs'
-    });
-    element(by.css('.sky-flyout .sky-flyout-btn-close')).click();
-  });
-
-  it('should match previous screenshot when row iterators are disabled', (done) => {
-    SkyHostBrowser.get('visual/flyout');
-    SkyHostBrowser.setWindowBreakpoint('lg');
+  function validateDisabledIteratorButtons(size: SkyHostBrowserBreakpoint, done: DoneFn): void {
+    SkyHostBrowser.setWindowBreakpoint(size);
     element(by.css('#open-flyout-with-iterators-disabled')).click();
     expect('body').toMatchBaselineScreenshot(done, {
-      screenshotName: 'flyout-no-iterators-lg'
+      screenshotName: getScreenshotName(`flyout-no-iterators-${size}`)
     });
-    element(by.css('.sky-flyout .sky-flyout-btn-close')).click();
-  });
+  }
 
-  it('should match previous screenshot when row iterator are disabled (screen: xs)', (done) => {
-    SkyHostBrowser.get('visual/flyout');
-    SkyHostBrowser.setWindowBreakpoint('xs');
-    element(by.css('#open-flyout-with-iterators-disabled')).click();
-    expect('body').toMatchBaselineScreenshot(done, {
-      screenshotName: 'flyout-no-iterators-xs'
-    });
-    element(by.css('.sky-flyout .sky-flyout-btn-close')).click();
-  });
-
-  it('should match previous screenshot when flyout is fullscreen (screen: xs)', (done) => {
-    SkyHostBrowser.get('visual/flyout');
-    SkyHostBrowser.setWindowBreakpoint('xs');
+  function validateFullScreen(size: SkyHostBrowserBreakpoint, done: DoneFn): void {
+    SkyHostBrowser.setWindowBreakpoint(size);
     element(by.css('#open-flyout-fullscreen')).click();
     expect('body').toMatchBaselineScreenshot(done, {
-      screenshotName: 'flyout-fullscreen'
+      screenshotName: getScreenshotName(`flyout-fullscreen-${size}`)
     });
-    element(by.css('.sky-flyout .sky-flyout-btn-close')).click();
+  }
+
+  function validateResponsiveContainer(size: SkyHostBrowserBreakpoint, done: DoneFn): void {
+    // Since we're testing the responsive container inside the flyout,
+    // the browser should always run with a large breakpoint.
+    SkyHostBrowser.setWindowBreakpoint('lg');
+    element(by.css(`#open-responsive-flyout-${size}`)).click();
+    expect('body').toMatchBaselineScreenshot(done, {
+      screenshotName: getScreenshotName(`flyout-responsive-${size}`)
+    });
+  }
+
+  function closeFlyout(): void {
+    element(by.css('.sky-flyout-btn-close')).click();
+  }
+
+  function runTests(): void {
+    it('should match previous screenshot', (done) => {
+      validateFlyout('lg', done);
+    });
+
+    it('should match previous screenshot (screen: xs)', (done) => {
+      validateFlyout('xs', done);
+    });
+
+    it('should handle absolutely positioned items inside the flyout', (done) => {
+      validateDropdownInFlyout('lg', done);
+    });
+
+    it('should handle absolutely positioned items inside the flyout (screen: xs)', (done) => {
+      validateDropdownInFlyout('xs', done);
+    });
+
+    it('should match previous screenshot when row iterators are enabled', (done) => {
+      validateIteratorButtons('lg', done);
+    });
+
+    it('should match previous screenshot when row iterators are enabled (screen: xs)', (done) => {
+      validateIteratorButtons('xs', done);
+    });
+
+    it('should match previous screenshot when row iterators are disabled', (done) => {
+      validateDisabledIteratorButtons('lg', done);
+    });
+
+    it('should match previous screenshot when row iterator are disabled (screen: xs)', (done) => {
+      validateDisabledIteratorButtons('xs', done);
+    });
+
+    it('should match previous screenshot when flyout is fullscreen (screen: xs)', (done) => {
+      validateFullScreen('xs', done);
+    });
+
+    it('should match previous screenshot when the flyout contains responsive content (flyout: xs)',
+      (done) => {
+        validateResponsiveContainer('xs', done);
+      });
+
+    it('should match previous screenshot when the flyout contains responsive content (flyout: sm)',
+      (done) => {
+        validateResponsiveContainer('sm', done);
+      });
+
+    it('should match previous screenshot when the flyout contains responsive content (flyout: md)',
+      (done) => {
+        validateResponsiveContainer('md', done);
+      });
+
+    it('should match previous screenshot when the flyout contains responsive content (flyout: lg)',
+      (done) => {
+      validateResponsiveContainer('lg', done);
+    });
+  }
+  //#endregion
+
+  beforeEach(async () => {
+    currentTheme = undefined;
+    currentThemeMode = undefined;
+
+    await SkyHostBrowser.get('visual/flyout');
   });
 
-  it('should match previous screenshot when the flyout contains responsive content (flyout: xs)',
-    (done) => {
-      SkyHostBrowser.get('visual/flyout');
-      SkyHostBrowser.setWindowBreakpoint('lg');
-      element(by.css('#open-responsive-flyout-xs')).click();
-      expect('body').toMatchBaselineScreenshot(done, {
-        screenshotName: 'flyout-responsive-xs'
-      });
-      element(by.css('.sky-flyout .sky-flyout-btn-close')).click();
-    });
+  afterEach(() => {
+    closeFlyout();
+  });
 
-  it('should match previous screenshot when the flyout contains responsive content (flyout: sm)',
-    (done) => {
-      SkyHostBrowser.get('visual/flyout');
-      SkyHostBrowser.setWindowBreakpoint('lg');
-      element(by.css('#open-responsive-flyout-sm')).click();
-      expect('body').toMatchBaselineScreenshot(done, {
-        screenshotName: 'flyout-responsive-sm'
-      });
-      element(by.css('.sky-flyout .sky-flyout-btn-close')).click();
-    });
-
-  it('should match previous screenshot when the flyout contains responsive content (flyout: md)',
-    (done) => {
-      SkyHostBrowser.get('visual/flyout');
-      SkyHostBrowser.setWindowBreakpoint('lg');
-      element(by.css('#open-responsive-flyout-md')).click();
-      expect('body').toMatchBaselineScreenshot(done, {
-        screenshotName: 'flyout-responsive-md'
-      });
-      element(by.css('.sky-flyout .sky-flyout-btn-close')).click();
-    });
-
-  it('should match previous screenshot when the flyout contains responsive content (flyout: lg)',
-    (done) => {
-      SkyHostBrowser.get('visual/flyout');
-      SkyHostBrowser.setWindowDimensions(1250, 800);
-      element(by.css('#open-responsive-flyout-lg')).click();
-      expect('body').toMatchBaselineScreenshot(done, {
-        screenshotName: 'flyout-responsive-lg'
-      });
-      element(by.css('.sky-flyout .sky-flyout-btn-close')).click();
-    });
+  runTests();
 
   it('should match previous screenshot when the flyout contains responsive content (screen: xs)',
-    (done) => {
-      SkyHostBrowser.get('visual/flyout');
-      SkyHostBrowser.setWindowBreakpoint('xs');
-      element(by.css('#open-responsive-flyout-lg')).click();
-      expect('body').toMatchBaselineScreenshot(done, {
-        screenshotName: 'flyout-responsive-lg-screen-xs'
-      });
-      element(by.css('.sky-flyout .sky-flyout-btn-close')).click();
+  (done) => {
+    SkyHostBrowser.setWindowBreakpoint('xs');
+    element(by.css('#open-responsive-flyout-lg')).click();
+    expect('body').toMatchBaselineScreenshot(done, {
+      screenshotName: 'flyout-responsive-lg-screen-xs'
     });
+  });
+
+  describe('when modern theme', () => {
+    beforeEach(async () => {
+      await selectTheme('modern', 'light');
+    });
+
+    runTests();
+  });
+
+  describe('when modern theme in dark mode', () => {
+    beforeEach(async () => {
+      await selectTheme('modern', 'dark');
+    });
+
+    runTests();
+  });
 });
