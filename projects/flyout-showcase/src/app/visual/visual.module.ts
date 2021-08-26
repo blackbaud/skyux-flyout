78c8cf22bad8bcd7e8db24e04e1c95683cc5a69e
Alex <alex.kingman@blackbaud.com>
2021-08-26 15:00:21 -0400

    Next > Adopt Angular CLI (#119)
    * Support skyHref (#116)
    
    * Support skyHref
    
    * Reset screenshots
    
    * Build #skyux-flyout-push-925343508-58933: Added new baseline screenshots. [ci skip]
    
    * Release 4.2.0 (#117)
    
    * initial eject
    
    * visual tests
    
    * coverage ignores
    
    * added if check to e2e tests
    
    * using NoopAnimationsModule
    
    Co-authored-by: John H White <750350+johnhwhite@users.noreply.github.com>
    Co-authored-by: Blackbaud Sky Build User <sky-build-user@blackbaud.com>
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>

---
+++ projects/flyout-showcase/src/app/visual/visual.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlyoutVisualComponent } from './flyout/flyout-visual.component';
+import { FlyoutResponsiveDemoContentComponent } from './flyout/flyout-responsive-demo-content.component';
+import { VisualComponent } from './visual.component';
+import { SkyDropdownModule } from '@skyux/popovers'
+import { SkyDocsToolsModule } from '@skyux/docs-tools';
+import { FlyoutResponsiveDemoComponent } from './flyout/flyout-responsive-demo.component';
+import { SkyModalModule } from '@skyux/modals';
+import { FlyoutModalDemoComponent } from './flyout/flyout-modal.component';
+import { SkyInfiniteScrollModule } from '@skyux/lists';
+import { FlyoutDemoComponent } from './flyout/flyout-demo.component';
+import { RouterModule } from '@angular/router';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    FlyoutDemoComponent,
+    FlyoutModalDemoComponent,
+    FlyoutResponsiveDemoContentComponent,
+    FlyoutResponsiveDemoComponent,
+    FlyoutVisualComponent,
+    VisualComponent
+  ],
+  //  Using NoopAnimationsModule for e2e tests.
+  //  Replace this with BrowserAnimationsModule to see animations.
+  imports: [
+    CommonModule,
+    NoopAnimationsModule,
+    RouterModule,
+    SkyDocsToolsModule,
+    SkyDropdownModule,
+    SkyInfiniteScrollModule,
+    SkyModalModule
+  ]
+})
+export class VisualModule { }
