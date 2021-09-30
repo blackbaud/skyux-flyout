92ae5ef6f1be786b20c6bd62559313a9ea7c919d
John White <750350+johnhwhite@users.noreply.github.com>
2021-09-30 09:58:58 -0400

    Merge 5.0.0 next (#126)
    * 5.0.0-next
    
    * Remove BrowserAnimationsModule from FlyoutModule (#114)
    
    * Remove BrowserAnimationsModule from FlyoutModule
    
    * Release 5.0.0-alpha.0
    
    * Changed singletons to use providedIn: root and changed unnecessary singletons to component providers (#115)
    
    * Changed singletons to use providedIn: root and removed unnecessary singletons
    
    * revert
    
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>
    
    * Merge from master; 5.0.0-beta.0 release (#118)
    
    * Merge from master; 5.0.0-beta.0 release
    
    * changelog
    
    * pr feedback
    
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>
    
    * Next > Adopt Angular CLI (#119)
    
    * Support skyHref (#116)
    
    * Support skyHref
    
    * Reset screenshots
    
    * Build #skyux-flyout-push-925343508-58933: Added new baseline screenshots. [ci skip]
    
    * Release 4.2.0 (#117)
    
    * initial eject
    
    * visual tests
    
    * coverage ignores
    
    * added if check to e2e tests
    
    * using NoopAnimationsModule
    
    Co-authored-by: John H White <750350+johnhwhite@users.noreply.github.com>
    Co-authored-by: Blackbaud Sky Build User <sky-build-user@blackbaud.com>
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>
    
    * 5.0.0-beta.1 release (#121)
    
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>
    
    * Build #skyux-flyout-push-1171742798-37905: Added new baseline screenshots. [ci skip]
    
    * Removed unused dependencies; Added router and theme as peer dependencies; added new theme swithcer (#122)
    
    * removed dependencies
    
    * skyux upgrade
    
    * removed baselines
    
    * update dependencies
    
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>
    
    * Build #skyux-flyout-push-1222289758-71352: Added new baseline screenshots. [ci skip]
    
    * 5.0.0-beta.2 release (#123)
    
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>
    
    * 5.0.0 beta.3 release (#124)
    
    * dependency fix
    
    * upgrade
    
    * Fix test coverage
    
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>
    Co-authored-by: Blackbaud-SteveBrush <steve.brush@blackbaud.com>
    
    * 5.0.0-beta.4 release; remove @Directive decorator from SkyFlyoutInstance (#125)
    
    * Remove directive decorator from SkyFlyoutInstance
    
    * changelog; removed ngdestroy
    
    * update
    
    * moved complete calls to the close method
    
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>
    
    * Workflow and package updates
    
    * Exact dependencies
    
    * Exact dependencies
    
    * @skyux/packages as prod dependency
    
    Co-authored-by: Paul Crowder <paul.crowder@blackbaud.com>
    Co-authored-by: Alex <alex.kingman@blackbaud.com>
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>
    Co-authored-by: Blackbaud Sky Build User <sky-build-user@blackbaud.com>
    Co-authored-by: Blackbaud-SteveBrush <steve.brush@blackbaud.com>

---
+++ projects/flyout-showcase/src/app/visual/flyout/flyout-responsive-demo.component.ts
@@ -0,0 +1,15 @@
+import {
+  Component
+} from '@angular/core';
+import { SkyFlyoutService } from 'projects/flyout/src/public-api';
+
+@Component({
+  selector: 'app-flyout-responsive-demo',
+  templateUrl: './flyout-responsive-demo.component.html',
+  providers: [SkyFlyoutService]
+})
+export class FlyoutResponsiveDemoComponent {
+
+  constructor() { }
+
+}
---
+++ projects/flyout-showcase/src/app/visual/flyout/flyout-visual.component.ts
@@ -0,0 +1,135 @@
+import {
+  Component,
+  OnDestroy
+} from '@angular/core';
+
+import {
+  Subject
+} from 'rxjs';
+
+import {
+  takeUntil
+} from 'rxjs/operators';
+
+import {
+  FlyoutDemoComponent
+} from './flyout-demo.component';
+
+import {
+  FlyoutResponsiveDemoComponent
+} from './flyout-responsive-demo.component';
+
+import {
+  FlyoutDemoContext
+} from './flyout-demo-context';
+import { SkyFlyoutInstance, SkyFlyoutService } from 'projects/flyout/src/public-api';
+
+@Component({
+  selector: 'app-flyout-visual',
+  templateUrl: './flyout-visual.component.html',
+  styleUrls: ['./flyout-visual.component.scss']
+})
+export class FlyoutVisualComponent implements OnDestroy {
+  public users: {id: string, name: string}[] = [
+    { id: '1', name: 'Sally' },
+    { id: '2', name: 'John' },
+    { id: '3', name: 'David' },
+    { id: '4', name: 'Janet' }
+  ];
+
+  public flyout: SkyFlyoutInstance<any>;
+
+  public showButtons = true;
+
+  private ngUnsubscribe = new Subject();
+
+  constructor(
+    private flyoutService: SkyFlyoutService
+  ) { }
+
+  public ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
+
+  public openFlyout(record: any): void {
+    this.flyout = this.flyoutService.open(FlyoutDemoComponent, {
+      providers: [{
+        provide: FlyoutDemoContext,
+        useValue: record
+      }],
+      defaultWidth: 500
+    });
+
+    this.flyout.closed.subscribe(() => {
+      this.showButtons = true;
+    });
+
+    this.showButtons = false;
+  }
+
+  public openFlyoutWithIterators(record: any, previousButtonDisabled: boolean, nextButtonDisabled: boolean): void {
+    this.flyout = this.flyoutService.open(FlyoutDemoComponent, {
+      providers: [{
+        provide: FlyoutDemoContext,
+        useValue: record
+      }],
+      defaultWidth: 500,
+      showIterator: true,
+      iteratorPreviousButtonDisabled: previousButtonDisabled,
+      iteratorNextButtonDisabled: nextButtonDisabled
+    });
+
+    this.flyout.iteratorPreviousButtonClick
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(() => {
+        console.log('previous clicked');
+      });
+
+    this.flyout.iteratorNextButtonClick
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(() => {
+        console.log('next clicked');
+      });
+
+    this.flyout.closed.subscribe(() => {
+      this.showButtons = true;
+    });
+
+    this.showButtons = false;
+  }
+
+  public openFlyoutWithFullscreenAvailable(record: any): void {
+    this.flyout = this.flyoutService.open(FlyoutDemoComponent, {
+      providers: [{
+        provide: FlyoutDemoContext,
+        useValue: record
+      }],
+      minWidth: 600,
+      defaultWidth: 800
+    });
+
+    this.flyout.closed.subscribe(() => {
+      this.showButtons = true;
+    });
+
+    this.showButtons = false;
+  }
+
+  public openResponsiveFlyout(width: number): void {
+    this.flyout = this.flyoutService.open(FlyoutResponsiveDemoComponent, {
+      defaultWidth: width,
+      maxWidth: 5000
+    });
+
+    this.flyout.closed.subscribe(() => {
+      this.showButtons = true;
+    });
+
+    this.showButtons = false;
+  }
+
+  public toggleButtons() {
+    this.showButtons = !this.showButtons;
+  }
+}
