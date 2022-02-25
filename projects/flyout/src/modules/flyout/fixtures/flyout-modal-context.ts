ae2db752157c8153c0c07a1c722d46256cd5d152
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2022-02-25 13:38:11 -0500

    Updated README.md to point contributors to the monorepo [ci skip]

--- projects/flyout/src/modules/flyout/fixtures/flyout-modal-context.ts
+++
@@ -1,4 +0,0 @@
-export class SkyFlyoutModalFixtureContext {
-  public valueA: string;
-  public eventMessage?: string;
-}
--- projects/flyout/src/modules/flyout/types/flyout-close-args.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Arguments used when closing a flyout programatically.
- */
-export interface SkyFlyoutCloseArgs {
-  /**
-   * Indicates if the `SkyFlyoutBeforeCloseHandler` should be ignored when closing a flyout.
-   */
-  ignoreBeforeClose?: boolean;
-}
